fd70e151c9b69ab47d1f77e617bfe4bba81f65e7
colining <18810782954@163.com>
2021-03-25 16:45:09 +0800

    import @material-ui/core & use CssBaseline in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.global.css';
+import { CssBaseline } from '@material-ui/core';
 import Home from './component/Home';
 import SideBar from './component/SideBar';
 
 export default function App() {
   return (
     <div className="main-body">
+      <CssBaseline />
       <SideBar />
       <Home />
     </div>
