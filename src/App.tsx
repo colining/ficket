e09785232ccd941fc0991247e34e10f2bda8a0a8
colining <18810782954@163.com>
2021-03-28 17:41:21 +0800

    replace search bar by material

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import './App.global.css';
 import { CssBaseline } from '@material-ui/core';
-import Home from './component/Home';
-import SideBar from './component/SideBar';
+import SearchAppBar from './component/SearchAppBar';
 
 export default function App() {
   return (
     <div className="main-body">
       <CssBaseline />
-      <SideBar />
-      <Home />
+      <SearchAppBar />
     </div>
   );
 }
