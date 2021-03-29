f7e3f802f29e4b40bf828db1b0d58c486e7250f0
colining <18810782954@163.com>
2021-03-29 19:23:15 +0800

    inject the js before webview showing

--- src/component/SearchAppBar.tsx
+++ src/component/SearchAppBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
@@ -97,10 +97,41 @@ const useStyles = makeStyles((theme: Theme) =>
     },
   })
 );
+const script = `
+const body = document.querySelector('body.active');
+const { children } = body;
+for (let i = 0; i < children.length; ) {
+  if (
+    children[i].tagName != 'SCRIPT' &&
+    children[i].className !== 'container'
+  ) {
+    console.log(i);
+    body.removeChild(children[i]);
+  } else {
+    i++;
+  }
+}
+const video = document.querySelector(
+  'body > div.container > div > div.col-lg-wide-75.col-md-wide-7.col-xs-1.padding-0 > div:nth-child(1) > div > div > div.myui-player__item.clearfix'
+);
+const container = document.querySelector('body > div.container');
+container.innerHTML = '';
+
+container.append(video);
+`;
 
 export default function SearchAppBar() {
+  const webView = useRef(WebView);
+  const [showWebView, setShowWebView] = useState(false);
+
   const classes = useStyles();
 
+  const handleLoad = () => {
+    console.log(webView.current);
+    webView.current.openDevTools();
+    webView.current.executeJavaScript(script);
+    setShowWebView(true);
+  };
   return (
     <div className={classes.root}>
       <AppBar position="fixed" className={classes.appBar}>
@@ -153,9 +184,14 @@ export default function SearchAppBar() {
       <main className={classes.content}>
         <Toolbar />
         <WebView
-          style={{ height: '100%' }}
+          ref={webView}
+          style={{
+            height: '100%',
+            visibility: showWebView ? 'visible' : 'hidden',
+          }}
           src="https://e.duboku.fun/vodplay/1953-1-15.html"
-          devtools={false}
+          onDidFinishLoad={handleLoad}
+          devtools
           plugins
         />
       </main>
