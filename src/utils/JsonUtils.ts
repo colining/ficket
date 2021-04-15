5c632937ba50124e6040e062afae0879413216ac
colining <18810782954@163.com>
2021-04-15 16:14:42 +0800

    [func] add source.json when package

--- src/utils/JsonUtils.ts
+++ src/utils/JsonUtils.ts
@@ -1,37 +1,38 @@
 import fs from 'fs';
 import jsonfile from 'jsonfile';
 import _ from 'lodash';
+import path from 'path';
 
-const path = 'source.json';
+const sourcePath = path.join(path.dirname(__dirname), 'source.json');
 
 export default function save(newData: any) {
-  if (!fs.existsSync(path)) {
-    fs.writeFileSync(path, JSON.stringify([]));
+  if (!fs.existsSync(sourcePath)) {
+    fs.writeFileSync(sourcePath, JSON.stringify([]));
   }
   const oldData = jsonfile
-    .readFileSync(path)
+    .readFileSync(sourcePath)
     .filter((item: any) => item.homepageUrl !== newData.homepageUrl);
-  jsonfile.writeFileSync(path, oldData.concat(newData), { spaces: 2 });
+  jsonfile.writeFileSync(sourcePath, oldData.concat(newData), { spaces: 2 });
 }
 
 export function read() {
-  return jsonfile.readFileSync(path);
+  return jsonfile.readFileSync(sourcePath);
 }
 
 export function update(data: any) {
-  jsonfile.writeFileSync(path, data, { spaces: 2 });
+  jsonfile.writeFileSync(sourcePath, data, { spaces: 2 });
   console.log('write');
 }
 
 export function importData(newData: any) {
-  if (!fs.existsSync(path)) {
-    fs.writeFileSync(path, JSON.stringify([]));
+  if (!fs.existsSync(sourcePath)) {
+    fs.writeFileSync(sourcePath, JSON.stringify([]));
   }
-  const oldData = jsonfile.readFileSync(path).filter((item: any) => {
+  const oldData = jsonfile.readFileSync(sourcePath).filter((item: any) => {
     return _.isEmpty(
       newData.filter((i: any) => i.homepageUrl === item.homepageUrl)
     );
   });
   console.log(oldData);
-  jsonfile.writeFileSync(path, oldData.concat(newData), { spaces: 2 });
+  jsonfile.writeFileSync(sourcePath, oldData.concat(newData), { spaces: 2 });
 }
