dce4be9ab2cd6694c9843db068f223549e32a7d5
colining <18810782954@163.com>
2021-04-27 13:10:12 +0800

    [function] add default theme in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import './App.global.css';
-import { CssBaseline } from '@material-ui/core';
+import {
+  createMuiTheme,
+  CssBaseline,
+  MuiThemeProvider,
+} from '@material-ui/core';
 import { HashRouter, Route } from 'react-router-dom';
+import { lightBlue, amber } from '@material-ui/core/colors';
 import Ficket from './component/Ficket';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: lightBlue[700],
+    },
+    secondary: {
+      main: amber[700],
+    },
+  },
+});
 
 export default function App() {
   return (
     <div className="main-body">
       <CssBaseline />
       <HashRouter>
-        <Route path="/" component={Ficket} />
+        <MuiThemeProvider theme={theme}>
+          <Route path="/" component={Ficket} />
+        </MuiThemeProvider>
       </HashRouter>
     </div>
   );
