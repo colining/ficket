0612e1a26f9fe3857890caa83e3b4bc1ec8d8556
colining <18810782954@163.com>
2021-06-15 14:11:38 +0800

    [func] add more check about null value

--- src/utils/SteamWorks.ts
+++ src/utils/SteamWorks.ts
@@ -1,6 +1,7 @@
 import greenworks from 'greenworks';
 import * as jsonfile from 'jsonfile';
 import React from 'react';
+import _ from 'lodash';
 
 export const WorkshopContext = React.createContext({
   workshopSource: [],
@@ -13,16 +14,26 @@ export default function getWorkShopItemsPathAndSetToState(setState: any) {
     greenworks.UserUGCListSortOrder.CreationOrderDesc,
     greenworks.UserUGCList.Subscribed,
     (items: any) => {
-      const workshopSource = items.map((item: any) =>
-        jsonfile.readFileSync(
+      let workshopSource = items.map((item: any) => {
+        if (_.isEmpty(greenworks.ugcGetItemInstallInfo(item.publishedFileId))) {
+          return null;
+        }
+        return jsonfile.readFileSync(
           greenworks.ugcGetItemInstallInfo(item.publishedFileId).folder
-        )
-      );
-      workshopSource.map((source: any) => {
-        source.workshopTag = true;
-        return source;
+        );
       });
-      setState({ workshopSource, loadSuccess: true });
+
+      if (!_.isNull(workshopSource)) {
+        workshopSource = workshopSource.filter(
+          (source: any) => !_.isNull(source)
+        );
+
+        workshopSource.map((source: any) => {
+          source.workshopTag = true;
+          return source;
+        });
+        setState({ workshopSource, loadSuccess: true });
+      }
     },
     (e: any) => {
       console.log(e);
