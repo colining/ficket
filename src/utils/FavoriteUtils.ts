8b7a3fa6d29a39073c93cb3e532b453f194fad06
colining <18810782954@163.com>
2021-07-07 16:04:55 +0800

    [func] save favorites.json with steam cloud

--- src/utils/FavoriteUtils.ts
+++ src/utils/FavoriteUtils.ts
@@ -1,10 +1,26 @@
+import greenworks from 'greenworks';
 import path from 'path';
 import fs from 'fs';
 import jsonfile from 'jsonfile';
 
 import { remote } from 'electron';
 
-const sourcePath = path.join(remote.app.getPath('userData'), 'favorites.json');
+const defaultSourcePath = path.join(
+  remote.app.getPath('userData'),
+  'favorites.json'
+);
+
+const getCloudSourcePath = (steamId: any) => {
+  return path.join(
+    remote.app.getPath('userData'),
+    'save',
+    steamId,
+    'favorites.json'
+  );
+};
+const sourcePath = greenworks.init()
+  ? getCloudSourcePath(greenworks.getSteamId().accountId.toString())
+  : defaultSourcePath;
 
 export default function saveFavorite(newData: any) {
   if (!fs.existsSync(sourcePath)) {
@@ -13,12 +29,25 @@ export default function saveFavorite(newData: any) {
   const oldData = jsonfile
     .readFileSync(sourcePath)
     .filter((item: any) => item.videoDetail !== newData.videoDetail);
-  jsonfile.writeFileSync(sourcePath, oldData.concat(newData), { spaces: 2 });
+  jsonfile.writeFileSync(sourcePath, oldData.concat(newData), {
+    spaces: 2,
+  });
+}
+
+function getFolder(s: string) {
+  return s.substring(0, s.lastIndexOf('\\'));
 }
 
 export function readFavorites() {
+  let defaultFavourites: any = [];
+
   if (!fs.existsSync(sourcePath)) {
-    fs.writeFileSync(sourcePath, JSON.stringify([]));
+    fs.mkdirSync(getFolder(sourcePath), { recursive: true });
+    if (fs.existsSync(defaultSourcePath)) {
+      defaultFavourites = jsonfile.readFileSync(defaultSourcePath);
+    }
+    fs.writeFileSync(sourcePath, '');
+    jsonfile.writeFileSync(sourcePath, defaultFavourites, { spaces: 2 });
   }
   return jsonfile.readFileSync(sourcePath).reverse();
 }
