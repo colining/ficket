2527afc4a55545d26984dce0baa14fdad0fc5bbe
colining <18810782954@163.com>
2021-07-15 09:59:23 +0800

    [func] fix the article from future bug

--- src/utils/BookUtils.ts
+++ src/utils/BookUtils.ts
@@ -12,9 +12,16 @@ const defaultArticle = {
   author: '异时空来者',
   article: '读读书，总是好的嘛',
 };
+
+function notFuture(date: string) {
+  const today = new Date().toISOString().slice(0, 10).replace(/-/g, '');
+  return parseInt(date, 10) <= parseInt(today, 10);
+}
+
 export default function getDailyReading() {
   const files = fs
     .readdirSync(dailyReadingFolder)
+    .filter((date) => notFuture(date))
     .sort((a, b) => parseInt(b, 10) - parseInt(a, 10));
   try {
     const filename = path.join(dailyReadingFolder, files[0], 'article.json');
