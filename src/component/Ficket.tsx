6d07795ae091915b9740c271cfff4f103e78662f
colining <18810782954@163.com>
2021-08-03 12:20:30 +0800

    [func] add a tour of app by react-joyride

--- src/component/Ficket.tsx
+++ src/component/Ficket.tsx
@@ -1,5 +1,7 @@
 import React, { useContext, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Joyride, { ACTIONS, CallBackProps, STATUS } from 'react-joyride';
+import { Fab } from '@material-ui/core';
 import MainContainer from './MainContainer';
 import DrawerContainer from './DrawerContainer';
 import AppBarContainer from './AppBarContainer';
@@ -13,6 +15,11 @@ const useStyles = makeStyles({
     height: 'inherit',
     width: 'inherit',
   },
+  tour: {
+    position: 'absolute',
+    right: '1%',
+    top: '120px',
+  },
 });
 
 export default function Ficket(props: any) {
@@ -22,6 +29,35 @@ export default function Ficket(props: any) {
   const [playlists, setPlaylists] = useState([]);
   const [currentInfo, setCurrentInfo] = useState({});
   const [open, setOpen] = useState(false);
+  const [steps] = useState([
+    {
+      content: <h2>让我们开始Ficket 教程吧~</h2>,
+      locale: { skip: <strong aria-label="skip">跳过</strong> },
+      placement: 'center',
+      target: 'body',
+      disableBeacon: true,
+      hideCloseButton: true,
+    },
+    {
+      target: '#sourceButton',
+      content: <h2>点击这里可以设置视频源</h2>,
+      disableBeacon: true,
+      hideCloseButton: true,
+    },
+    {
+      target: '#sourceList',
+      content: <h2>您可以在这里设置源，并设置是否启用</h2>,
+      disableBeacon: true,
+      hideCloseButton: true,
+    },
+    {
+      target: '#root > div > div > header > div > div > div> input',
+      content: <h2>在这里搜索您想看的剧</h2>,
+      disableBeacon: true,
+      hideCloseButton: true,
+    },
+  ]);
+  const [run, setRun] = useState(false);
 
   const handleCurrentInfoChange = (info: any) => {
     setCurrentInfo(info);
@@ -43,8 +79,43 @@ export default function Ficket(props: any) {
     props.history.push('/main/searchResult');
   };
 
+  const handleJoyrideCallback = (data: CallBackProps) => {
+    const { status, action, index } = data;
+    const finishedStatuses: string[] = [STATUS.FINISHED, STATUS.SKIPPED];
+    if (finishedStatuses.includes(status)) {
+      setRun(false);
+    }
+    if (action === ACTIONS.CLOSE) {
+      setRun(false);
+    }
+    if (index === 1) {
+      props.history.push('/main/source/list');
+    }
+  };
   return (
     <div className={classes.root}>
+      <Joyride
+        callback={handleJoyrideCallback}
+        continuous
+        disableOverlayClose
+        run={run}
+        scrollToFirstStep
+        showProgress
+        showSkipButton
+        steps={steps}
+        spotlightClicks
+        styles={{
+          options: {
+            zIndex: 10000,
+          },
+        }}
+        locale={{
+          back: '返回',
+          last: '结束',
+          next: '下一步',
+          skip: '跳过',
+        }}
+      />
       <AppBarContainer keyPress={keyPress} />
       <DrawerContainer
         playlists={playlists}
@@ -65,6 +136,14 @@ export default function Ficket(props: any) {
         }}
         message="search...."
       />
+      <Fab
+        color="primary"
+        aria-label="add"
+        className={classes.tour}
+        onClick={() => setRun(true)}
+      >
+        教程
+      </Fab>
     </div>
   );
 }
