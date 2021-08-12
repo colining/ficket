7fc5ab87ab8e53251eb4a13b95c8b708da824f2e
colining <18810782954@163.com>
2021-08-12 15:24:13 +0800

    [func] add tiny-async-pool && @types/react-infinite-scroller

--- type.d.ts
+++ type.d.ts
@@ -1,5 +1,6 @@
 declare module 'react-electron-web-view';
 declare module 'autoresponsive-react';
+declare module 'tiny-async-pool';
 declare module 'jsonfile';
 declare module 'json-templates';
 declare module 'greenworks';
